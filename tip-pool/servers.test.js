c5961cdf47745f77d391de840cad0d6bc9bd02ed
Megan Buglio <megan.buglio@gmail.com>
2023-08-17 13:32:29 -0500

    adding files

---
+++ tip-pool/servers.test.js
@@ -0,0 +1,36 @@
+describe("Servers test (with setup and tear-down)", function() {
+  beforeEach(function () {
+    // initialization logic
+    serverNameInput.value = 'Alice';
+  });
+
+  it('should add a new server to allServers on submitServerInfo()', function () {
+    submitServerInfo();
+
+    expect(Object.keys(allServers).length).toEqual(1);
+    expect(allServers['server' + serverId].serverName).toEqual('Alice');
+  });
+
+  it('should not add a new server on submitServerInfo() with empty input', function(){
+    serverNameInput.value = '';
+    submitServerInfo();
+
+    expect(Object.keys(allServers).length).toEqual(0);
+  });
+
+  it('should update #servertable on updateServerTable()', function(){
+    submitServerInfo();
+    updateServerTable();
+
+    let curTdList = document.querySelectorAll('#serverTable tbody tr td');
+
+    expect(curTdList.length).toEqual(3);
+    expect(curTdList.length[0]).toEqual('Alice');
+    expect(curTdList.length[1]).toEqual('$0.00');
+    expect(curTdList.length[2]).toEqual('X');
+  });
+
+  afterEach(function() {
+    // teardown logic
+  });
+});
